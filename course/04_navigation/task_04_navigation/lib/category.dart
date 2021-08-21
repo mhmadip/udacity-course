4296ce5c6002b63c67059d41c4c46d547a16e07f
Mohammad Salim <mhmadip@gmail.com>
2021-08-22 01:38:06 +0300

    Update category.dart
    Adding null safety

--- course/04_navigation/task_04_navigation/lib/category.dart
+++ course/04_navigation/task_04_navigation/lib/category.dart
@@ -8,8 +8,8 @@ import 'package:flutter/material.dart';
 // @required is defined in the meta.dart package
 import 'package:meta/meta.dart';
 
-import 'package:task_04_navigation/converter_route.dart';
-import 'package:task_04_navigation/unit.dart';
+import 'package:flutter_navig/converter_route.dart';
+import 'package:flutter_navig/unit.dart';
 
 // We use an underscore to indicate that these variables are private.
 // See https://www.dartlang.org/guides/language/effective-dart/design#libraries
@@ -21,10 +21,10 @@ final _borderRadius = BorderRadius.circular(_rowHeight / 2);
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
 class Category extends StatelessWidget {
-  final String name;
-  final ColorSwatch color;
-  final IconData iconLocation;
-  final List<Unit> units;
+  final String? name;
+  final ColorSwatch? color;
+  final IconData? iconLocation;
+  final List<Unit>? units;
 
   /// Creates a [Category].
   ///
@@ -34,7 +34,6 @@ class Category extends StatelessWidget {
   // it doesn't check whether the object passed in is null. We check that
   // in the assert statement.
   const Category({
-    Key key,
     @required this.name,
     @required this.color,
     @required this.iconLocation,
@@ -42,8 +41,7 @@ class Category extends StatelessWidget {
   })  : assert(name != null),
         assert(color != null),
         assert(iconLocation != null),
-        assert(units != null),
-        super(key: key);
+        assert(units != null);
 
   /// Navigates to the [ConverterRoute].
   void _navigateToConverter(BuildContext context) {
@@ -91,7 +89,7 @@ class Category extends StatelessWidget {
                 ),
                 Center(
                   child: Text(
-                    name,
+                    name!,
                     textAlign: TextAlign.center,
                     style: Theme.of(context).textTheme.headline,
                   ),
